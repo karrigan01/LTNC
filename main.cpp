9d4338e392cb00f9932dbf710cffbb61b028bc57
karrigan01 <106960373+karrigan01@users.noreply.github.com>
2025-03-30 00:50:31 +0700

    Update main.cpp
    split to menu.h for new features

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include "music_background.h"
 #include "tetromino.h"
 #include "system.h"
+#include "menu.h"
 
 using namespace std;
 int DROP_SPEED = 500; // milliseconds
@@ -85,37 +86,6 @@ void initSDL(SDL_Window*& window, SDL_Renderer*& renderer,TTF_Font* &font) {
     SDL_Color white = {255, 255, 255};
     window = SDL_CreateWindow("Tetris", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-}
-bool showMenu(SDL_Renderer* renderer, TTF_Font* font, SDL_Texture* menuTexture) {
-    bool inMenu = true;
-    SDL_Event event;
-
-    while (inMenu) {
-        // Render the menu background image
-        SDL_RenderCopy(renderer, menuTexture, NULL, NULL);
-
-        // Render text
-        SDL_Color white = {255, 255, 255};
-        SDL_Surface* surface = TTF_RenderText_Solid(font, "Press ENTER to Start", white);
-        SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, surface);
-        SDL_Rect textRect = {SCREEN_WIDTH / 2 - surface->w / 2, SCREEN_HEIGHT / 2 - surface->h / 2, surface->w, surface->h};
-
-        SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
-
-        SDL_FreeSurface(surface);
-        SDL_DestroyTexture(textTexture);
-
-        SDL_RenderPresent(renderer);
-
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                return false; // Exit game
-            } else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_RETURN) {
-                inMenu = false; // Start game
-            }
-        }
-    }
-    return true;
 }
 void renderNextTetromino(SDL_Renderer* renderer, Tetromino nextTetromino) {
     int previewX = SCREEN_WIDTH - 200; // Adjust the position
