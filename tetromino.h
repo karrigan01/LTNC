21fa4365b5804bf6bc120cda6bb465d3d06cd5a8
karrigan01 <106960373+karrigan01@users.noreply.github.com>
2025-03-29 11:16:05 +0700

    Update tetromino.h
    fixed color

--- tetromino.h
+++ tetromino.h
@@ -9,7 +9,16 @@ using namespace std;
 const int GRID_WIDTH = 10;
 const int GRID_HEIGHT = 20;
 const int BLOCK_SIZE = 30;  // Size of each block in pixels
-
+SDL_Color colors[] = {
+    {255, 0, 0, 255},   // Red for shape 0
+    {0, 255, 0, 255},   // Green for shape 1
+    {0, 0, 255, 255},   // Blue for shape 2
+    {255, 255, 0, 255}, // Yellow for shape 3
+    {255, 165, 0, 255}, // Orange for shape 4
+    {128, 0, 128, 255}, // Purple for shape 5
+    {0, 255, 255, 255}, // Cyan for shape 6
+    {192, 192, 192, 255} // Gray for shape 7
+};
 // Tetromino shapes
 vector<vector<vector<int>>> tetrominoShapes = {
     {{1, 1, 1, 1}},  // I
@@ -18,15 +27,17 @@ vector<vector<vector<int>>> tetrominoShapes = {
     {{1, 1, 0}, {0, 1, 1}}, // Z
     {{1, 1, 1}, {0, 1, 0}}, // T
     {{1, 1, 1}, {1, 0, 0}}, // L
-    {{1, 1, 1}, {0, 0, 1}}  // J
+    {{1, 1, 1}, {0, 0, 1}},  // J
+    {{1, 1, 1}, {0, 0, 1}} //reverse L
 };
 
 struct Tetromino {
     vector<vector<int>> shape;
     int x, y;
-
+    SDL_Color color;
     Tetromino(int type) {
         shape = tetrominoShapes[type];
+        color=colors[type];
         x = GRID_WIDTH / 2 - shape[0].size() / 2;
         y = 0;
     }
@@ -53,13 +64,17 @@ struct Tetromino {
     void moveLeft(bool grid[GRID_HEIGHT][GRID_WIDTH]) {
         if (isValidPosition(x - 1, y, shape, grid)) {
             x--;
+            //return true;
         }
+        //return false;
     }
 
     void moveRight(bool grid[GRID_HEIGHT][GRID_WIDTH]) {
         if (isValidPosition(x + 1, y, shape, grid)) {
             x++;
+            //return true;
         }
+        //return false;
     }
 
     bool moveDown(bool grid[GRID_HEIGHT][GRID_WIDTH]) {
@@ -82,7 +97,9 @@ struct Tetromino {
 
         if (isValidPosition(x, y, rotated, grid)) {
             shape = rotated;
+            //return true;
         }
+        //return false;
     }
 
     // Lock Tetromino into grid when it can't move down
@@ -97,7 +114,7 @@ struct Tetromino {
     }
 };
 void drawTetromino(SDL_Renderer* renderer, Tetromino& tetromino) {
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);  // Red color for Tetromino
+    SDL_SetRenderDrawColor(renderer, tetromino.color.r, tetromino.color.g, tetromino.color.b, 255); //draw color for tetromino
 
     for (int i = 0; i < tetromino.shape.size(); i++) {
         for (int j = 0; j < tetromino.shape[i].size(); j++) {
