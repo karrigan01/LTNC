d087c607d74f7cfd0c0f84a44a772ba06b6507e7
karrigan01 <106960373+karrigan01@users.noreply.github.com>
2025-03-30 18:12:17 +0700

    Update setting.h

--- setting.h
+++ setting.h
@@ -11,7 +11,7 @@ struct Button {
     std::string text;
 };
 
-// Function to render a button
+
 void renderButton(SDL_Renderer* renderer, TTF_Font* font, Button button) {
     SDL_SetRenderDrawColor(renderer, button.color.r, button.color.g, button.color.b, 255);
     SDL_RenderFillRect(renderer, &button.rect);
@@ -32,27 +32,24 @@ void renderButton(SDL_Renderer* renderer, TTF_Font* font, Button button) {
     SDL_DestroyTexture(texture);
 }
 
-// Function to check if a button is clicked
 bool isButtonClicked(Button button, int mouseX, int mouseY) {
     return (mouseX >= button.rect.x && mouseX <= button.rect.x + button.rect.w &&
             mouseY >= button.rect.y && mouseY <= button.rect.y + button.rect.h);
 }
 int DROP_SPEED = 500; // milliseconds
-// Show settings menu
 void showSettings(SDL_Renderer* renderer, TTF_Font* font) {
     bool inSettings = true;
     SDL_Event event;
 
     // Define buttons
-    Button easyButton = {{250, 200, 200, 50}, {0, 255, 0, 255}, "Easy"};
-    Button mediumButton = {{250, 300, 200, 50}, {255, 165, 0, 255}, "Medium"};
-    Button hardButton = {{250, 400, 200, 50}, {255, 0, 0, 255}, "Hard"};
+    Button easyButton = {{150, 350, 180, 70}, {0, 255, 0, 255}, "Easy"};
+    Button mediumButton = {{320, 350, 180, 70}, {255, 165, 0, 255}, "Medium"};
+    Button hardButton = {{490, 350, 180, 70}, {255, 0, 0, 255}, "Hard"};
 
     while (inSettings) {
         SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
         SDL_RenderClear(renderer);
 
-        // Render "Press ESC to return" at the top-left corner
         SDL_Color white = {255, 255, 255};
         SDL_Surface* surface = TTF_RenderText_Solid(font, "Press ESC to return", white);
         SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
@@ -77,15 +74,12 @@ void showSettings(SDL_Renderer* renderer, TTF_Font* font) {
                 int x, y;
                 SDL_GetMouseState(&x, &y);
                 if (isButtonClicked(easyButton, x, y)) {
-                    // Implement Easy difficulty logic later
                     DROP_SPEED=500;
                 }
                 else if (isButtonClicked(mediumButton, x, y)) {
-                    // Implement Medium difficulty logic later
                     DROP_SPEED=400;
                 }
                 else if (isButtonClicked(hardButton, x, y)) {
-                    // Implement Hard difficulty logic later
                     DROP_SPEED=200;
                 }
             }
